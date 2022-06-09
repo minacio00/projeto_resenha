65a897998515177ee63cef7b21239e5160a1f5e1
minacio00 <mateus_aruana@hotmail.com>
2022-06-09 03:39:59 -0300

    sidebar done

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState, useRef } from "react";
+import { ArrowLeftIcon, MenuIcon, UserAddIcon,
+    UserIcon, HomeIcon, CogIcon, QuestionMarkCircleIcon, LogoutIcon
+}
+ from "@heroicons/react/solid";
+
+
+export const Sidebar = () => {
+   const [hidden, setHidden] = useState(true)
+   // // useEffect(() => {
+   // //   setHidden(!isHidden)
+   // // }, [isHidden])
+   const handleClick = ()=>{
+      setHidden(!hidden)
+      // alert(hidden)
+   }
+   function useOutsideAlerter(ref) {
+      useEffect(() => {
+        /**
+         * Alert if clicked on outside of element
+         */
+        function handleClickOutside(event) {
+          if (ref.current && !ref.current.contains(event.target) && !hidden) {
+            // alert("You clicked outside of me!");
+            setHidden(!hidden)
+          }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+          // Unbind the event listener on clean up
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, [ref,hidden]);
+    }
+
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef);
+   
+    return(
+       <>
+       <MenuIcon className={`w-6 h-6 m-3 hover:cursor-pointer `} onClick={handleClick}/>
+         <div ref={wrapperRef} className={` indent-0 ${hidden ? 'hidden' : " " } w-3/5 bg-slate-50 px-2 text-center fixed pt-8 pin-t pin-l h-screen border-r-4 border-grey-dark text-black`}
+        >
+               <div className={` relative mx-auto`}
+               >
+                  
+                  <ArrowLeftIcon className=" mb-4 w-5 h-5 hover:cursor-pointer hover:bg-gray-100" onClick={handleClick}/>
+                  <ul className="list-reset flex flex-col text-center" >
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className=" block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 md:pr-3"></i>
+                           <span className=" pb-1 text-sm md:text-base text-grey-dark block">Cadastro Proprietário </span>
+                           
+                           </a>
+                        </li>
+                        <UserAddIcon className="w-5 h-5"/>
+                     </div>
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className="block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 "></i>
+                           <span className="pb-1 text-sm md:text-base text-grey-dark block">Busca Proprietário </span>
+                           
+                           </a>
+                        </li>
+                        <UserIcon className="w-5 h-5"/>
+                     </div>
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className="block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 md:pr-3"></i>
+                           <span className="pb-1 text-sm md:text-base text-grey-dark block">Cadastro Equino </span>
+                           
+                           </a>
+                        </li>
+                        <UserAddIcon className="w-5 h-5"/>
+                     </div>
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className="block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 md:pr-3"></i>
+                           <span className="pb-1 text-sm md:text-base text-grey-dark block">Configurações </span>
+                           
+                           </a>
+                        </li>
+                        <CogIcon className="w-5 h-5"/>
+                     </div>
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className="block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 md:pr-3"></i>
+                           <span className="pb-1 text-sm md:text-base text-grey-dark block">Ajuda </span>
+                           
+                           </a>
+                        </li>
+                        <QuestionMarkCircleIcon className="w-5 h-5"/>
+                     </div>
+                     <div className="flex pb-4 hover:bg-gray-100">
+                        <li className="mr-3 flex-1 text-left">
+                           <a href="/controle" className="block py-1 md:py-3 pl-1 align-middle text-grey-darkest no-underline "
+                           >
+                           <i className="fas fa-link pr-0 md:pr-3"></i>
+                           <span className="pb-1 text-sm md:text-base text-grey-dark block">Sair </span>
+                           
+                           </a>
+                        </li>
+                        <LogoutIcon className="w-5 h-5"/>
+                     </div>
+                  </ul>
+               </div>
+            </div>
+         </>
+    )
+}
--- src/pages/controleResenha.jsx
+++ src/pages/controleResenha.jsx
@@ -1,23 +1,40 @@
-
+import {PencilIcon, TrashIcon,MenuIcon} from '@heroicons/react/solid';
+import { useEffect, useState } from 'react';
+import {Sidebar} from "../components/Sidebar";
 const ControleResenha = () => {
+    // const [isOpen, setIsOpen] = useState(false);
+    // const toggle = () => {
+    //     // alert(isOpen);
+    //     setIsOpen(!isOpen);
+    // };
 
     return (
-        <div className="flex-col space-y-8">
-            <header className="bg-indigo-600 h-12 p-3 indent-10
+        <div className="flex-col space-y-6"> 
+            <header className="bg-indigo-600 h-12 indent-10
             text-white font-bold text-left">
-                Controle de Resenha
+                <div className='flex'>
+                    <Sidebar />
+                    <span className='p-3'>
+                        Controle de Resenha
+                    </span>
+                </div>
             </header>
-            <body className="space-y-4">
+            <body className="space-y-6">
                 <div className="flex items-center justify-center space-x-4 px-1">
                     <p className="w-20">Proprietário</p>
                     <select className="overflow-hidden w-80 border-0 rounded-md bg-gray-100">
                         <option value="fruit">teste teste testeteste teste</option>
                         <option value="vegetable">Vegetable</option>
                         <option value="meat">Meat</option>
+                        <option value="vegetable">Vegetable</option>
+                        <option value="meat">Meat</option>
+                        <option value="vegetable">Vegetable</option>
+                        <option value="meat">Meat</option>
+    
                     </select>
                 </div>
                 <div className="flex items-center justify-center space-x-4 px-1">
-                    <p className="w-20" >Animal</p>
+                    <p className="w-20" >Nome do animal</p>
                     <select className="overflow-hidden w-80 border-0 rounded-md bg-gray-100">
                         <option className="overflow-hidden" value="fruit">
                             animalimal 
@@ -27,16 +44,45 @@ const ControleResenha = () => {
                     </select>
                 </div>
                 {/* maybe this will break in big screens, fix it if u wanna */}
-                <div className="flex items-center justify-between max-w-screen-sm px-1 space-y-6">
+                <div className="flex items-center justify-between max-w-screen-sm px-1 ">
                     <p>Cor</p>
                     <select className="overflow-hidden border-0 rounded-md bg-gray-100">
                         <option value="azul">azul</option>
                     </select>
 
-                    <p>Sub cor</p>
+                    <p>Sub-cor</p>
                     <select className="overflow-hidden border-0 rounded-md bg-gray-100">
                         <option value="azul">azul</option>
                     </select>
+                </div>
+
+                <div>
+                    <table className="table-auto border border-black w-full text-xs">
+                        <thead>
+                            <tr className="bg-gray-200">
+                                <th className="border-2 border-black">Proprietário</th>
+                                <th className="border-2 border-black shrink">Nome do <br/> animal</th>
+                                <th className="border-2 border-black" >Cor</th>
+                                <th className="border-2 border-black">Sub-cor</th>
+                                <th className="border-2 border-black">Editar</th>
+                                <th className="border-2 border-black">Excluir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className='border-2 border-black'>1</td>
+                                <td className='border-2 border-black'>2</td>
+                                <td className='border-2 border-black'>3</td>
+                                <td className='border-2 border-black'>4</td>
+                                <td className='border-2 border-black content-center px-1 w-2'>
+                                    <PencilIcon className='w-5 h-5' />
+                                </td>
+                                <td className='border-2 border-black px-1 w-2' >
+                                    <TrashIcon className='w-5 h-5'/>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
 
                 {/*TODO: adicionar ul no restante da pagina */}
